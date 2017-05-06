7c9b9195c2eec3740efb8ce79120e5db49a34725
Omri Spector <omri.spector@gmail.com>
2017-05-06 18:09:48 +0300

    service implemented

--- src/main/java/com/develeap/consumer/BidService.java
+++ src/main/java/com/develeap/consumer/BidService.java
@@ -1,8 +1,14 @@
 package com.develeap.consumer;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.net.URI;
+
+import static com.develeap.consumer.ReservationStatus.OK;
 
 @Service
 public class BidService {
@@ -15,6 +21,37 @@ public class BidService {
   }
 
   public boolean buyArtwork(BuyRequest buyRequest) {
-    throw new NotImplementedException();
+    boolean success = false;
+    ReservationResponse reservationResponse = reserve(buyRequest);
+
+    boolean reserved = reservationResponse.status==OK;
+    if (reserved) {
+      // do the rest of the buy stuff
+      System.out.println("*** Reservation ID is " + reservationResponse.reservationId);
+      success = true;
+    }
+
+    return success;
   }
+
+  private ReservationResponse reserve(BuyRequest buyRequest) {
+    // Call remote service
+    ResponseEntity<ReservationResponse> response = this.restTemplate.exchange(
+        RequestEntity
+            .post(URI.create("http://localhost:" + port + "/reserve"))
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(buyRequest),
+        ReservationResponse.class);
+
+    return response.getBody();
+  }
+}
+
+class ReservationResponse {
+  public ReservationStatus status;
+  public long reservationId;
+}
+
+enum ReservationStatus {
+  OK, ALREADY_RESERVED, NOT_AVAILABLE
 }
--- src/test/java/com/develeap/consumer/BidServiceTest.java
+++ src/test/java/com/develeap/consumer/BidServiceTest.java
@@ -10,6 +10,17 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureStubRunner(workOffline = true, ids = "com.develeap.scc:producer:+:stubs:8090")
+//@AutoConfigureStubRunner(repositoryRoot = "http://nexus.producer-company.com", ids = "com.develeap.scc:producer:+:stubs:8090")
 public class BidServiceTest {
 
+  @Test public void should_accept_attempt_to_buy_available_painting() throws Exception {
+    BidService theUnit = new BidService(new RestTemplate());
+    Assert.assertTrue(theUnit.buyArtwork(new BuyRequest("123", "Ben Johnson")));
+  }
+
+  @Test public void should_fail_to_buy_non_existing_painting() throws Exception {
+    BidService theUnit = new BidService(new RestTemplate());
+    Assert.assertFalse(theUnit.buyArtwork(new BuyRequest("no-such-painting", "Ben Johnson")));
+  }
 }
